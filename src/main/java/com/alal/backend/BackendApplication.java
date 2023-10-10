4f05aaf7306fa283f120c821c53ec2360cb33bcd
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-10-10 09:30:12 +0900

    초기 설정

--- src/main/java/com/alal/backend/BackendApplication.java
+++ src/main/java/com/alal/backend/BackendApplication.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@PropertySource(value = { "classpath:database/database.properties" })
-@PropertySource(value = { "classpath:oauth2/oauth2.properties" })
-@PropertySource(value = { "classpath:swagger/springdoc.properties" })
+//@PropertySource(value = {"classpath:database/database.yml"})
+//@PropertySource(value = {"classpath:oauth2/oauth2.yml"})
+//@PropertySource(value = {"classpath:swagger/springdoc.yml"})
 public class BackendApplication {
 
     public static void main(String[] args) {

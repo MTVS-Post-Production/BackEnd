48ca97b61bcaf33bd8db77d7c66421f30e8d8051
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-10-16 20:42:44 +0900

    [Test] : 클라이언트에서 Mp4 파일 전달받아 Flask 전송 및 응답으로 Url 찾기 테스트 코드 작성

--- src/test/java/com/alal/backend/service/user/MotionServiceTest.java
+++ src/test/java/com/alal/backend/service/user/MotionServiceTest.java
@@ -1,8 +1,11 @@
 package com.alal.backend.service.user;
 
 import com.alal.backend.payload.response.ViewResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +13,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.mock.web.MockMultipartFile;
 
+import javax.swing.text.View;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,8 +26,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class MotionServiceTest {
 
-    @Autowired
+    @InjectMocks
     private MotionService motionService;
+
+    @Mock
+    private MotionService motionServiceMock;
+
 
     @DisplayName("sample, example을 받아 정상적으로 gif, fbx 주소를 반환하는지")
     @Test
@@ -30,8 +39,6 @@ class MotionServiceTest {
         // given
         List<String> messages = Arrays.asList("chunsic", "dance");
         Pageable pageable = PageRequest.of(0, 30);
-
-        MotionService motionServiceMock = Mockito.mock(MotionService.class);
 
         List<ViewResponse> expectedResponses = new ArrayList<>();
         ViewResponse viewResponse = ViewResponse.builder().build();
@@ -45,6 +52,35 @@ class MotionServiceTest {
         Mockito.when(motionServiceMock.findGifByMessages(messages, pageable)).thenReturn(expectedPages);
         // when
         Page<ViewResponse> resultPages = motionServiceMock.findGifByMessages(messages, pageable);
+
+        // then
+        assertEquals(expectedPages.getContent(), resultPages.getContent());
+    }
+
+    @DisplayName("mp4 파일을 정상적으로 업로드하여 gif, fbx 주소를 가져오는지")
+    @Test
+    void findUrlByUploadMp4() {
+        // given
+        MockMultipartFile mp4File = new MockMultipartFile(
+                "sample",
+                "sample.mp4",
+                "video/mp4",
+                "my mp4 content".getBytes()
+        );
+
+        Pageable pageable = PageRequest.of(0, 30);
+
+        List<ViewResponse> expectedResponses = new ArrayList<>();
+        ViewResponse viewResponse = ViewResponse.builder().build();
+        viewResponse.setGifUrls(Arrays.asList("sampleGif1", "sampleGif2"));
+        expectedResponses.add(viewResponse);
+
+        Page<ViewResponse> expectedPages = new PageImpl<>(expectedResponses, pageable, expectedResponses.size());
+
+        Mockito.when(motionServiceMock.findUrlByUploadMp4(mp4File,pageable)).thenReturn(expectedPages);
+
+        // when
+        Page<ViewResponse> resultPages = motionServiceMock.findUrlByUploadMp4(mp4File, pageable);
 
         // then
         assertEquals(expectedPages.getContent(), resultPages.getContent());

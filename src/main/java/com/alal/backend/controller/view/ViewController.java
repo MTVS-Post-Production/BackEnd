f2845d56be62efe2343a6608d5635953490bd4cd
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-10-24 00:20:38 +0900

    [Refactor] : 유저 히스토리 컬럼 추가 및 fbxUrl, gifUrl 담는 코드 수정

--- src/main/java/com/alal/backend/controller/view/ViewController.java
+++ src/main/java/com/alal/backend/controller/view/ViewController.java
@@ -1,12 +1,20 @@
 package com.alal.backend.controller.view;
 
+import com.alal.backend.config.security.token.CurrentUser;
+import com.alal.backend.config.security.token.UserPrincipal;
+import com.alal.backend.domain.entity.user.User;
 import com.alal.backend.payload.request.auth.FbxRequest;
+import com.alal.backend.payload.request.auth.FlaskRequest;
 import com.alal.backend.payload.response.FbxResponse;
 import com.alal.backend.payload.response.FlaskResponse;
 import com.alal.backend.payload.response.ViewResponse;
 import com.alal.backend.service.user.MotionService;
+import java.net.HttpCookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
@@ -28,50 +36,46 @@ public class ViewController {
 
     private final MotionService motionService;
 
-    private Page<ViewResponse> lastViewResponses;
-
     @GetMapping
-    public String main(Model model) {
-        if (lastViewResponses != null) {
-            model.addAttribute("motionUrls", lastViewResponses);
-        }
+    public String main(Model model, @CurrentUser UserPrincipal userPrincipal,
+                       @PageableDefault(size = 30) Pageable pageable) {
+        Long userId = userPrincipal.getId();
+        Page<ViewResponse> viewResponses = motionService.createViewResponse(userId, pageable);
+
+        model.addAttribute("motionUrls", viewResponses);
+
         return "main/imagePage";
     }
 
     // 클라이언트에서 동영상 파일(mp4)을 받아 Flask 서버와 통신하여 문자열 리스트를 받음
     @PostMapping("/video")
-    public String videoPost(@RequestParam("file") MultipartFile file,
-                            @PageableDefault(size = 30) Pageable pageable,
-                            Model model
-                            ) {
-        Page<ViewResponse> viewResponse = motionService.findUrlByUploadMp4(file, pageable);
-        lastViewResponses = viewResponse;
+    @ResponseBody
+    public String videoPost(@RequestBody FlaskRequest flaskRequest, @CurrentUser UserPrincipal userPrincipal) {
+        Long userId = userPrincipal.getId();
+        motionService.findUrlByUploadMp4(flaskRequest, userId);
 
-        model.addAttribute("motionUrls", viewResponse);
-
-        return "main/imagePage";
+        return "success";
     }
 
     // 클라이언트에서 음성 파일을 받아 Flask 서버와 통신 후 변조된 음성 파일 응답
     @PostMapping("/voice")
     @ResponseBody
-    public ResponseEntity<FlaskResponse> voicePost(@RequestParam("file") MultipartFile file
-    ) {
-        FlaskResponse flaskResponse = motionService.uploadAndRespondWithAudioFileSuccess(file);
+    public ResponseEntity<FlaskResponse> voicePost(@RequestBody FlaskRequest flaskRequest
+                                                   ) {
+        FlaskResponse flaskResponse = motionService.uploadAndRespondWithAudioFileSuccess(flaskRequest);
         return ResponseEntity.ok(flaskResponse);
     }
 
     // 클라이언트에서 문자열을 받아 Flask 서버와 통신하여 문자열 리스트 반환받음
-    @PostMapping("/message")
-    public String messagePost(@RequestBody String message, Model model,
-                              @PageableDefault(size = 30) Pageable pageable){
-        Page<ViewResponse> viewResponse = motionService.findGifByMessages(message, pageable);
-        lastViewResponses = viewResponse;
-
-        model.addAttribute("motionUrls", viewResponse);
-
-        return "main/imagePage";
-    }
+//    @PostMapping("/message")
+//    public String messagePost(@RequestBody String message, Model model,
+//                              @PageableDefault(size = 30) Pageable pageable){
+//        Page<ViewResponse> viewResponse = motionService.findGifByMessages(message, pageable);
+//
+//        model.addAttribute("motionUrls", viewResponse);
+//
+//        return "main/imagePage";
+//    }
 
     // 웹에서 다운로드 버튼 클릭 시 /view/result?fbxUrl=... 로 리다이렉트
     @PostMapping("/send/fbx")
@@ -86,21 +90,4 @@ public class ViewController {
         headers.setLocation(redirectUri);
         return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
-
-    // 다운로드 버튼을 통해 리다이렉트한 주소에 대한 GET 요청
-    @GetMapping("/result")
-    public String result(@RequestParam("fbxUrl") String fbxUrl, Model model) {
-        model.addAttribute("fbxUrl", fbxUrl);
-        return "main/result";
-    }
-
-
-
-    // 웹에서 다운로드 버튼 클릭 시, 언리얼 엔진으로 전송
-//    @PostMapping("/send/fbx")
-//    @ResponseBody
-//    public ResponseEntity<FbxResponse> send(@RequestBody FbxRequest fbxRequest){
-//        return ResponseEntity.ok(FbxResponse.fromFbxUrl(fbxRequest));
-//    }
-
 }

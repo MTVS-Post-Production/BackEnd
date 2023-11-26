729da75d9ee0640c12facb3ee7650b28ba02c6e0
bbbbooo <qlqlzbxkzl@naver.com>
2023-11-26 18:18:19 +0900

    refactor : 전체 조회 시, script를 반환하도록 변경

--- src/main/java/com/alal/backend/domain/dto/response/ReadProjectsResponse.java
+++ src/main/java/com/alal/backend/domain/dto/response/ReadProjectsResponse.java
@@ -14,12 +14,14 @@ public class ReadProjectsResponse {
     private Long projectId;
     private String projectName;
     private String posterUrl;
+    private String script;
 
     public static ReadProjectsResponse fromEntity(Project project) {
         return ReadProjectsResponse.builder()
                 .projectId(project.getProjectId())
                 .projectName(project.getProjectName())
                 .posterUrl(project.getPoster())
+                .script(project.getScript())
                 .build();
     }
 }
